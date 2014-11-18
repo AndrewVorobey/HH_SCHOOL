442f78144476ec28bc8f16733781c23b4d4d6b3d
Andrew <andrew_vorobey@mail.ru>
2014-11-18 23:19:29 +0300

    Fixed - filePath gets from command line

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static int main(String[] args) throws IOException {
         String filename = "log.txt";
         LogParser lotParser = new LogParser();
-        lotParser.parseLog(filename);//must get args[0]
+        lotParser.parseLog(args[0]);
         System.out.println();
         for(Map.Entry<Integer,Integer> s: lotParser.computeUsersTimes().entrySet()) {
             System.out.println("id:"+ s.getValue().toString() + " time:" + s.getKey().toString());
