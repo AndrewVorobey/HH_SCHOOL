e9ca3cdf6e18faaaf2d1eb185ce8e41cd19ff790
Andrew <andrew_vorobey@mail.ru>
2015-01-07 23:49:44 +0300

    nio

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,22 +1,98 @@
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
 
 /**
  * Created by andrew on 19.11.14.
  */
 public class Main {
-    static ServerSocket server;
-    public static void main(String[] args){
-        try {
-            server = new ServerSocket(3000, 0,
-                     InetAddress.getByName("localhost"));
-        } catch (IOException e) {
-            System.out.println(e.toString() + "\nTry again with a new hosts name");
-            return;
-        }
-        System.out.println("Server started");
-        Clients.StartToListen(server);
+    static private InetAddress hostAddress;
+    static private int port = 3000;
 
+    private static ServerSocketChannel serverChannel;
+
+    static private Selector selector;
+
+    public static void main(String[] args) {
+
+        ByteBuffer Buffer = ByteBuffer.allocate(8192);
+        try {
+            hostAddress = InetAddress.getByName("localhost");
+            selector = initSelector();
+            while (true) {
+                try {
+                    if (selector.select() == 0)
+                        continue;
+
+                    Iterator selectedKeys = selector.selectedKeys().iterator();
+                    while (selectedKeys.hasNext()) {
+                        SelectionKey key = (SelectionKey) selectedKeys.next();
+                        selectedKeys.remove();
+
+                        if (!key.isValid()) {
+                            continue;
+                        }
+
+                        if (key.isAcceptable()) {
+                            System.out.println(key);
+                            accept(key);
+                        } else if ((key.readyOps() & SelectionKey.OP_READ) ==
+                                SelectionKey.OP_READ) {
+                            SocketChannel sc = (SocketChannel) key.channel();
+                            sc.read(Buffer);
+                            for (SelectionKey ToKey : selector.selectedKeys()) {
+                                if (ToKey != key) {
+                                    //ToKey.interestOps(SelectionKey.OP_WRITE);
+                                    ((SocketChannel) ToKey.channel()).write(Buffer);
+                                    //ToKey.interestOps(SelectionKey.OP_READ);
+                                }
+                            }
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Selector initSelector() throws IOException {
+
+        Selector socketSelector = SelectorProvider.provider().openSelector();
+
+
+        serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+
+
+        InetSocketAddress isa = new InetSocketAddress(hostAddress, port);
+        serverChannel.socket().bind(isa);
+
+        serverChannel.register(socketSelector, SelectionKey.OP_ACCEPT);
+
+        return socketSelector;
+    }
+
+    private static void accept(SelectionKey key) throws IOException {
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        Socket socket = socketChannel.socket();
+        socketChannel.configureBlocking(false);
+
+        socketChannel.register(selector, SelectionKey.OP_READ);
     }
 }
